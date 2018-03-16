44334f434130aae7646f46a4cfcbdff4aefada51
MorganChdz <you@example.com>
2018-03-16 11:33:07 +0100

    step 4

--- src/AppBundle/Controller/DomainController.php
+++ src/AppBundle/Controller/DomainController.php
@@ -124,9 +124,10 @@ class DomainController extends FOSRestController
             $request->headers->set('Authorization', $all['Authorization']);
         }
     }
-     if ($this-> getUserApi()) throw new \Symfony\Component\Security\Core\Exception\InsufficientAuthenticationException;
-      if ( $this-> getUserApi()->getId() != $domain->getUser()->getId()) throw new \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
     $token = $request->headers->get('Authorization');
+    if ($this-> getUserApi()) throw new Exception("Error Processing Request", 1);
+     new \Symfony\Component\Security\Core\Exception\InsufficientAuthenticationException;
+      if ( $this-> getUserApi()->getId() != $domain->getUser()->getId()) throw new \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
       $form = $this->createForm(TranslationType::class, $request->request->all());
       $form->submit($request->request->all());
       if($form->isValid()){
